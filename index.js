8d8b90b89f49cffb5700479c910704cb7e5d4119
User <abbymoraa876@gmail.com>
2025-09-03 09:18:37 +0300

    updated employee keys

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+  name: "Sam",
+  streetAddress: "11 Broadway"
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  // returns a new object with the original + updated key/value
+  return {
+    ...employee,
+    [key]: value
+  };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value; // changes original object
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee }; // clone first
+  delete newEmployee[key];
+  return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
